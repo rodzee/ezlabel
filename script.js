76400a753237717768a558a01079d9a80ac982b0
Rodrigo Souza <rodzee@Rodrigos-MBP.home>
2020-09-16 07:16:26 -0400

    All files have been updated and cleaned up

--- script.js
+++ script.js
@@ -1,24 +1,30 @@
-const labels = document.querySelector('#label-container');
-const brQuantity = document.querySelector('#breaker-quantity');
+const labelContainer = document.getElementById('label-container');
+const brQuantity = document.getElementById('breaker-quantity');
+const labelInput = document.getElementById('label-name-input');
 
-// GENERATES ALL NUMBERS AND INPUT FIELDS
+// =============================================================================
+// GENERATES ALL NUMBERS AND INPUT FIELDS ======================================
+// =============================================================================
 let genFields = () => {
-    let numb = [...Array(parseInt(brQuantity.value) + 1).keys()].slice(1);
+    let numbers = [...Array(parseInt(brQuantity.value) + 1).keys()].slice(1);
 
     // generates the numberd input fields
-    let listArr = numb.map((e) => `<li><span>${e}</span>` + `<input type="text" class="label-input"></li>`);
-    labels.innerHTML = listArr.join('');
-    // this added Class is been used to style in CSS
-    labels.classList.add('label-numbers');
+    let listArr = numbers.map((number) => `<li><span>${number}</span> <input type="text" class="label-input"></li>`);
+    labelContainer.innerHTML = listArr.join('');
+    //! this added Class is been used to style in CSS
+    labelContainer.classList.add('label-numbers');
 };
 
 genFields();
 
-// PRINT
+// =============================================================================
+// PRINT =======================================================================
+// =============================================================================
 function printPage() {
     printJS({
-        printable: 'label-container',
+        printable: 'label-list',
         type: 'html',
+        documentTitle: labelInput.value,
         css: 'print-style.css',
         targetStyles: ['*'],
     });
